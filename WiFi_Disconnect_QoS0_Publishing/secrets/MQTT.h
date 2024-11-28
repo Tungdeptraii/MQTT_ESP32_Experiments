5407fb6e480a3525d983e17894e23b21ea6e1401
le tung <118238775+Tungdeptraii@users.noreply.github.com>
2024-11-28 14:04:57 +0700

    Delete WiFi_Disconnect_QoS0_Publishing/secrets directory

--- WiFi_Disconnect_QoS0_Publishing/secrets/MQTT.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-namespace MQTT
-{
-    const char *broker = "7715d2e4aae646d08e0c9f0bd8cc815f.s1.eu.hivemq.cloud";  
-    const int port = 8883;  //
-    const char *username = "tungdz";  
-    const char *password = "12345687";  
-}
